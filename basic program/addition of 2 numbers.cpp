0d03e4f9d8a95d824aa1c6eab9719d59a2c22403
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2023-10-01 10:46:19 +0530

    Add files via upload

---
+++ basic program/addition of 2 numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cout <<"Enter two Numbers : ";
+
+    cin>> a >> b;
+
+    sum = a+b;
+
+    cout<<endl<<a<<"+"<<b<<"="<<sum<<endl;
+
+    return 0;
+}
+
