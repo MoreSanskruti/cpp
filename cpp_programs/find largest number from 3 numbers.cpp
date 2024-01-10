1f632e3cc05284f5d0bdbfaf87ea3bb9655acc29
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2024-01-10 21:20:39 +0530

    Add files via upload

---
+++ cpp_programs/find largest number from 3 numbers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "Enter the three numbers :" << endl;
+	cin >> a >> b >> c;
+
+	if (a >= b)
+    {
+		  if (a >= c)
+		  {
+			cout << "The Largest Among Three Numbers is : "<< a << endl;
+		  }
+
+    else
+		  {
+			cout << "The Largest Among Three Numbers is : "<< c << endl;
+		  }
+    }
+	else
+        {
+		    if (b >= c)
+		  {
+			cout << "The Largest Among Three Numbers is : "<< b << endl;
+          }
+    else
+          {
+           cout << "The Largest Among Three Numbers is : "<< c << endl;
+		  }
+	    }
+	return 0;
+}
+
