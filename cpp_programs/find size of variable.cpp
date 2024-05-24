7354f27c5902883a6a377f8ee8764b1abfd1d33e
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2024-05-24 20:31:14 +0530

    Add files via upload

---
+++ cpp_programs/find size of variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Size Of Char: "<<sizeof(char)<<" byte"<<endl;
+    cout<<"Size Of Int: "<<sizeof(int)<<" byte"<<endl;
+    cout<<"Size Of Float: "<<sizeof(float)<<" byte"<<endl;
+    cout<<"Size Of Double: "<<sizeof(double)<<" byte"<<endl;
+
+    return 0;
+}
+
