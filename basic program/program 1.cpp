ac0e508041345d25857a3a4aef5dba368ec8cdf1
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2024-06-15 19:22:47 +0530

    concept of class and object

---
+++ basic program/program 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+public:
+    string name;
+};
+int main()
+{
+    student s;
+    s.name="SHUBHAM";
+    cout<<"student name : "<<s.name;
+    return 0;
+}
